fd062994935d75b8a96d6a76c6036bb0c42f993d
Niko Arellano <narellano@my.bcit.ca>
2018-08-02 00:19:24 -0700

    Initialize project

---
+++ src/app/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAaY1Wrj39WwN63Qf3DwoEO1RJ895A7xeA",
+  authDomain: "marketplace-5635e.firebaseapp.com",
+  databaseURL: "https://marketplace-5635e.firebaseio.com",
+  projectId: "marketplace-5635e",
+  storageBucket: "marketplace-5635e.appspot.com",
+  messagingSenderId: "504059842173"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export const firebaseAuth = firebase.auth();
+export const firebaseDb = firebase.database();
